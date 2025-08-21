c81376acef18a828f2f6983df52d5330d685dc25
Diogo Czervinski <160793892+diogo-czervinski@users.noreply.github.com>
2025-08-21 10:10:28 -0300

    entity user

---
+++ backtcc/src/Enuns/TipoUser.enum.ts
@@ -0,0 +1,5 @@
+export enum TipoUser{
+    ADIMIN = "ADMIN",
+    PRODUTOR = "PRODUTOR",
+    TAREFEIRO = "TAREFEIRO"
+}
---
+++ backtcc/src/User/Entity/user.entity.ts
@@ -0,0 +1,28 @@
+import { TipoUser } from "src/Enuns/TipoUser.enum";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Users{
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    tel: string;
+
+    @Column({
+        type: "enum",
+        enum: TipoUser,
+        default: TipoUser.PRODUTOR
+    })
+    role: TipoUser;
+
+}
